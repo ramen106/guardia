1ccea3faff0bf0b0d60a2a1d36551d81a109e2ed
unknown <ar.sameh1106@gmail.com>
2026-02-26 00:53:48 +0200

    Removed redundant imports

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.Group;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
