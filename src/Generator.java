49b79ca3d8ccb5668da3b7e5480cdeb5663b8c19
unknown <ar.sameh1106@gmail.com>
2026-02-26 00:53:26 +0200

    Refactored and improved generator logic

--- src/Generator.java
+++ src/Generator.java
@@ -1,49 +1,46 @@
 import java.security.SecureRandom;
 
 public class Generator {
+    private static final SecureRandom secRand = new SecureRandom();
+
     private static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String lower = "abcdefghijklmnopqrstuvwxyz";
     private static final String numbers = "0123456789";
     private static final String symbols = "!@#$%^&*()_+-=[]{}|";
-    private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|";
+    private static final String characters = upper + lower + numbers + symbols;
 
     public static String generatePassword(int length)
     {
-        SecureRandom secRand = new SecureRandom();
-        StringBuilder password = new StringBuilder();
+        StringBuilder password = new StringBuilder(length);
         
-        // Random indices to get random upper, lower, numbers, symbols from
-        // And random indices to place them at in the resulting password string
-        int[] indices = new int[4];
-        int[] indicesToPlace = new int[4];
-        int maxSubstringLength = upper.length();
+        // Putting all the substrings into an array to loop through
+        // And pick a random character from each
+        String[] pools = {upper, lower, numbers, symbols};
+        int maxStringLength = characters.length();
 
-        for(int i = 0; i < 4; i++)
+        for(String pool : pools)
         {
-            indices[i] = secRand.nextInt(0, maxSubstringLength);
-            indicesToPlace[i] = secRand.nextInt(0, length);
+            int maxSubstringLength = pool.length();
+            int randomIdx = secRand.nextInt(0, maxSubstringLength);
+            password.append(pool.charAt(randomIdx));
         }
         
-        // Wrap around if the resulting number too high
-        indices[2] %= numbers.length();
-        indices[3] %= symbols.length();
-        
-        // Fetch the randomly chosen required characters
-        char[] chosenInitialChars = {upper.charAt(indices[0]), lower.charAt(indices[1]), numbers.charAt(indices[2]), symbols.charAt(indices[3])};
-
-        for(int i = 0; i < length; i++)
+        // Generate the rest of the password
+        for(int i = 4; i < length; i++)
         {
-            int randomCharIdx = secRand.nextInt(0, 82);
+            int randomCharIdx = secRand.nextInt(0, maxStringLength);
             char randomChar = characters.charAt(randomCharIdx);
             password.append(randomChar);
         }
 
-        // Overwrite the indices we generated earlier with the chosen random characters
-        // To ensure at least 1 upper, 1 lower, 1 symbol, 1 number
-
+        // Swap the first 4 indices with 4 other random indices
+        // To ensure at least 1 upper, 1 lower, 1 symbol, 1 number placed at random points
         for(int i = 0; i < 4; i++)
         {
-            password.setCharAt(indicesToPlace[i], chosenInitialChars[i]);
+            int swapIdx = secRand.nextInt(length);
+            char temp = password.charAt(i);
+            password.setCharAt(i, password.charAt(swapIdx));
+            password.setCharAt(swapIdx, temp);
         }
 
         return password.toString();
